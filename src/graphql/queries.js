f498291a9daab05852d0336653c4b970ac6c2ab5
MinHee <dylandang.dev@gmail.com>
2023-04-08 16:10:25 +0700

    Complete Caching on Apollo Client

---
+++ src/graphql/queries.js
@@ -0,0 +1,113 @@
+import { ApolloClient, gql, InMemoryCache } from "@apollo/client";
+import { getAccessToken } from "../auth";
+
+const GRAPHQL_URL = "http://localhost:9000/graphql";
+
+const client = new ApolloClient({
+  uri: GRAPHQL_URL,
+  cache: new InMemoryCache(),
+});
+
+const JOB_DETAIL_FRAGMENT = gql`
+  fragment JobDetail on Job {
+    title
+    company {
+      id
+      name
+    }
+    description
+  }
+`;
+
+const JOB_QUERY = gql`
+  query jobQuery($id: ID!) {
+    job(id: $id) {
+      ...JobDetail
+    }
+  }
+  ${JOB_DETAIL_FRAGMENT}
+`;
+
+export async function createJob(input) {
+  const mutation = gql`
+    mutation CreateJobMutation($input: CreateJobInput!) {
+      job: createJob(input: $input) {
+        ...JobDetail
+      }
+    }
+    ${JOB_DETAIL_FRAGMENT}
+  `;
+  const variables = { input };
+  // const headers = {
+  //   Authorization: "Bearer " + getAccessToken(),
+  // };
+  const context = {
+    headers: { Authorization: "Bearer " + getAccessToken() },
+  };
+  const {
+    data: { job },
+  } = client.mutate({
+    mutation,
+    variables,
+    context,
+    // result (the 2nd param in update) is equal to data: {job}
+    update: (cache, { data: { job } }) => {
+      cache.writeQuery({
+        query: JOB_QUERY,
+        variables: { id: job.id },
+        data: { job },
+      });
+    },
+  });
+  console.log(job);
+  return job;
+}
+
+export async function getJob(id) {
+  const variables = { id };
+  const {
+    data: { job },
+  } = await client.query({ query: JOB_QUERY, variables });
+  return job;
+}
+
+export async function getJobs() {
+  const query = gql`
+    query JobQuery {
+      jobs {
+        id
+        title
+        company {
+          id
+          name
+        }
+      }
+    }
+  `;
+  const {
+    data: { jobs },
+  } = await client.query({ query, fetchPolicy: "network-only" });
+  return jobs;
+}
+
+export async function getCompany(id) {
+  const query = gql`
+    query getCompany($id: ID!) {
+      company(id: $id) {
+        id
+        name
+        description
+        jobs {
+          title
+          description
+        }
+      }
+    }
+  `;
+  const variables = { id };
+  const {
+    data: { company },
+  } = await client.query({ query, variables });
+  console.log(company);
+  return company;
+}
